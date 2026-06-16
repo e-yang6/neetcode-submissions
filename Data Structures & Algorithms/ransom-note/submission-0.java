8903f243c61942e356e34bbe6916db50b07dda96
Ethan Yang <ttethanyang@gmail.com>
2026-06-16 19:42:22 -0400

    Add: ransom-note - submission-0

---
+++ Data Structures & Algorithms/ransom-note/submission-0.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        HashMap<Character, Integer> count = new HashMap<>();
+        
+        for(char c : magazine.toCharArray()) {
+            count.put(c, count.getOrDefault(c, 0) + 1);
+        }
+        
+        for(char c : ransomNote.toCharArray()) {
+            if (count.getOrDefault(c, 0) == 0) {
+                return false;
+            }
+            
+            count.put(c, count.get(c) - 1);
+        }
+        
+        return true;
+    }
+}
